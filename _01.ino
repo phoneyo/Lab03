0e868ad9fa541895f27388efd6ba2276edc6d709
phoneyo <asdzxc4563@gmail.com>
2017-03-13 19:48:53 +0800

    Add files via upload

---
+++ _01.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Hello");
+  Serial.println("World");
+}
+void loop()
+{
+  char val;
+  if(Serial.available())
+  {
+    val = Serial.read();
+    switch(val)
+    {
+      
+      case'0':
+      digitalWrite(13,LOW);
+      Serial.println("LED OFF");
+      break;
+      case'1':
+      digitalWrite(13,HIGH);
+      Serial.println("LED ON");
+    }
+  }
+}
